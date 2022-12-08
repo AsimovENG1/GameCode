5875eb0ea8d7a4859c7c5874979f8026bd4c3bb5
TobyMeehan <toby@tobymeehan.com>
2022-12-08 11:44:56 +0000

    Add game button
    Added basic scene2d button to navigate to game screen

--- core/src/com/asimov/piazzapanic/MainMenuScreen.java
+++ core/src/com/asimov/piazzapanic/MainMenuScreen.java
@@ -1,53 +1,64 @@
 package com.asimov.piazzapanic;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class MainMenuScreen implements Screen {
+public class MainMenuScreen extends ScreenAdapter {
     final PiazzaPanic game;
 
-    OrthographicCamera camera;
+    private Stage stage;
 
     public MainMenuScreen(final PiazzaPanic game) {
         this.game = game;
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 600);
-    }
+        stage = new Stage(new ScreenViewport(), game.batch);
 
-    @Override
-    public void show() {
+        TextButtonStyle style = new TextButtonStyle();
+        style.font = game.font;
 
-    }
+        TextButton gameButton = new TextButton("Game Screen", style);
+        gameButton.setWidth(200);
+        gameButton.setHeight(50);
 
-    @Override
-    public void render(float delta) {
+        gameButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("", "Clicked");
+            }
+        });
 
+        stage.addActor(gameButton);
+
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
