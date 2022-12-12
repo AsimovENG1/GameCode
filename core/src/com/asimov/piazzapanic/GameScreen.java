619c62f55da48e3fe8ce302b4d34d131088458cf
TobyMeehan <toby@tobymeehan.com>
2022-12-12 14:27:36 +0000

    Add scene2d to game screen
    Replaced camera with stage on game screen

--- core/src/com/asimov/piazzapanic/GameScreen.java
+++ core/src/com/asimov/piazzapanic/GameScreen.java
@@ -4,21 +4,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameScreen extends ScreenAdapter {
     final PiazzaPanic game;
 
-    OrthographicCamera camera;
+    private Stage stage;
 
     public GameScreen(final PiazzaPanic game) {
         this.game = game;
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 600);
+        stage = new Stage(new ScreenViewport(), game.batch);
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
     }
 }
