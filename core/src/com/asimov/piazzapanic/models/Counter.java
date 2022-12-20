d7ec32dfc5de3d5d4e3336b36315277af1b93ee9
TobyMeehan <toby@tobymeehan.com>
2022-12-20 16:44:19 +0000

    Add customers
    Added customers and the serving counter

--- core/src/com/asimov/piazzapanic/models/Counter.java
+++
@@ -1,7 +0,0 @@
-package com.asimov.piazzapanic.models;
-
-import com.badlogic.gdx.utils.Array;
-
-public abstract class Counter {
-    public Array<Ingredient> items = new Array<>();
-}
---
+++ core/src/com/asimov/piazzapanic/models/Customer.java
@@ -0,0 +1,5 @@
+package com.asimov.piazzapanic.models;
+
+public class Customer {
+    public Recipe order;
+}
