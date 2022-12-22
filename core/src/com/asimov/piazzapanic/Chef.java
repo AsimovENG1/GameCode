baa4561db962c15869bc6e70b847255df0995e54
Emma Hogg <ejh647@york.ac.uk>
2022-12-22 19:05:25 +0000

    Three chefs added

--- core/src/com/asimov/piazzapanic/Chef.java
+++ core/src/com/asimov/piazzapanic/Chef.java
@@ -35,6 +35,9 @@ public class Chef extends InputAdapter implements Screen {
 
     float chef3x = 300;
     float chef3y = 300;
+
+    float chefnumber = 1;
+
     String direction1 = "Right"; //Starting direction of chef1
     String direction2 = "Right"; //Starting direction of chef2
     String direction3 = "Right"; //Starting direction of chef3
@@ -79,25 +82,61 @@ public class Chef extends InputAdapter implements Screen {
         batch.begin();
         stage.draw();
 
+        if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)){
+            chefnumber = 1;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.NUM_2)){
+            chefnumber = 2;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.NUM_3)){
+            chefnumber = 3;
+        }
+
         if (direction1 == "Right") { batch.draw(chef1Right, chef1x, chef1y);}
         else if (direction1 == "Left") {batch.draw(chef1Left, chef1x, chef1y);}
 
+        if (direction2 == "Right") { batch.draw(chef2Right, chef2x, chef2y);}
+        else if (direction2 == "Left") {batch.draw(chef2Left, chef2x, chef2y);}
+
+        if (direction3 == "Right") { batch.draw(chef3Right, chef3x, chef3y);}
+        else if (direction3 == "Left") {batch.draw(chef3Left, chef3x, chef3y);}
+
         if (Gdx.input.isKeyPressed(Input.Keys.W)){
-            chef1y += Gdx.graphics.getDeltaTime()*Speed;
+            if (chefnumber == 1) {chef1y += Gdx.graphics.getDeltaTime()*Speed;}
+            else if (chefnumber == 2) {chef2y += Gdx.graphics.getDeltaTime()*Speed;}
+            else if (chefnumber == 3) {chef3y += Gdx.graphics.getDeltaTime()*Speed;}
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.S)){
-            chef1y -= Gdx.graphics.getDeltaTime()*Speed;
+            if (chefnumber == 1) {chef1y -= Gdx.graphics.getDeltaTime()*Speed;}
+            else if (chefnumber == 2) {chef2y -= Gdx.graphics.getDeltaTime()*Speed;}
+            else if (chefnumber == 3) {chef3y -= Gdx.graphics.getDeltaTime()*Speed;}
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.A)){
-            chef1x -= Gdx.graphics.getDeltaTime()*Speed;
-            direction1 = "Left";
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            if (chefnumber == 1) {
+                chef1x -= Gdx.graphics.getDeltaTime() * Speed;
+                direction1 = "Left";
+            } else if (chefnumber == 2) {
+                chef2x -= Gdx.graphics.getDeltaTime() * Speed;
+                direction2 = "Left";
+            } else if (chefnumber == 3) {
+                chef3x -= Gdx.graphics.getDeltaTime() * Speed;
+                direction3 = "Left";
+            }
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.D)){
-            chef1x += Gdx.graphics.getDeltaTime()*Speed;
-            direction1 = "Right";
+            if (chefnumber == 1) {
+                chef1x += Gdx.graphics.getDeltaTime() * Speed;
+                direction1 = "Right";
+            } else if (chefnumber == 2) {
+                chef2x += Gdx.graphics.getDeltaTime() * Speed;
+                direction2 = "Right";
+            } else if (chefnumber == 3) {
+                chef3x += Gdx.graphics.getDeltaTime() * Speed;
+                direction3 = "Right";
+            }
         }
 
         final Sound sound2 = Gdx.audio.newSound(Gdx.files.internal("audio/Back-and-quit.wav"));
