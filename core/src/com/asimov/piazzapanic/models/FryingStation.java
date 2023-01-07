8817f844f2367acd01977f9a95c19c4576573d86
IsaacR89 <imrhodes8975@gmail.com>
2023-01-07 17:53:40 +0000

    added functionality
    added mechanism for grill need to discus how it works and how to put images with it

--- core/src/com/asimov/piazzapanic/models/FryingStation.java
+++ core/src/com/asimov/piazzapanic/models/FryingStation.java
@@ -1,16 +1,35 @@
 package com.asimov.piazzapanic.models;
 
 public class FryingStation extends CookingStation {
-//    Custom logic for frying station, e.g.
-//    public boolean canFry() {
-//
-//    }
-//
-//    public void fry() {
-//
-//        status = CookingStatus.complete;
-//    }
 
+    public void place() {
+        try {
+            ((Fryable)getIngredient()).ReadySwap();
+            ((Fryable)getIngredient()).stageIncrease();
+            Thread.sleep(5000);
+            ((Fryable)getIngredient()).ReadySwap();
+            }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public void flip() {
+        if (((Fryable)getIngredient()).ready() == true){
+            if (((Fryable)getIngredient()).state() == 1){
+                try {
+                    ((Fryable)getIngredient()).ReadySwap();
+                    ((Fryable)getIngredient()).stageIncrease();
+                    Thread.sleep(5000);
+                    ((Fryable)getIngredient()).ReadySwap();
+                    ((Fryable)getIngredient()).makeFried();
+                    status = CookingStatus.complete;
+                    }
+                catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            }
+        }
     @Override
     protected boolean canPlace(Ingredient ingredient) {
         return ingredient instanceof Fryable;
