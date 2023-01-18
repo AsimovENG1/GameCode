820c21a4d40874d5f0dcfd15564a6f7ea29dcbb9
IsaacR89 <imrhodes8975@gmail.com>
2023-01-18 10:27:43 +0000

    added models

--- core/src/com/asimov/piazzapanic/GrillStationActor.java
+++ core/src/com/asimov/piazzapanic/GrillStationActor.java
@@ -5,23 +5,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GrillStationActor extends CookingStationActor<FryingStation>{
-    // TODO: custom images
     private final Texture emptyGrillTexture = new Texture("emptyGrill.png"); // station is empty
 
-    private final Texture bunGrillTexture = new Texture("bunGrill.png"); // station has bun on
-    private final Texture bunGrillReadyTexture = new Texture("bunGrillReady.png"); // bun is ready to be flipped
-    private final Texture bunGrillFlipedTexture = new Texture("bunGrillFliped.png"); // bun is fliped
-    private final Texture bunGrillDoneTexture = new Texture("bunGrillDone.png"); // bun is ready to be taken off
-
-    private final Texture burgerGrillTexture = new Texture("burgerGrill.png"); // station has burger on
-    private final Texture burgerGrillReadyTexture = new Texture("burgerGrillReady.png"); // burger is ready to be flipped
-    private final Texture burgerGrillFlipedTexture = new Texture("burgerGrillFliped.png"); // burger is fliped
-    private final Texture burgerGrillDoneTexture = new Texture("burgerGrillDone.png"); // burger is ready to be taken off
+    private final Texture GrillTexture = new Texture("burgerGrill.png"); // station has burger on
+    private final Texture GrillReadyTexture = new Texture("burgerGrillReady.png"); // burger is ready to be flipped
+    private final Texture GrillFlipedTexture = new Texture("burgerGrillFliped.png"); // burger is fliped
+    private final Texture GrillDoneTexture = new Texture("burgerGrillDone.png"); // burger is ready to be taken off
     
     public GrillStationActor() {
         super(new FryingStation());
         setTexture(new TextureRegion(emptyGrillTexture));
     }
-    
+    @Override
+    public void placed() {
+        setTexture(new TextureRegion(GrillTexture));
+        model.place();
+        setTexture(new TextureRegion(GrillReadyTexture));
+    }
 
+    public void flip() {
+        setTexture(new TextureRegion(GrillFlipedTexture));
+        model.flip();
+        setTexture(new TextureRegion(GrillDoneTexture));
+    }
 }
