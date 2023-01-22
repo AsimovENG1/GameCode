a21ccf630fa8f1bbaf6ebe85e42199c3d1c24fae
IsaacR89 <imrhodes8975@gmail.com>
2023-01-22 13:38:10 +0000

    private Ingredient ingredient = null;
    bin almost done

--- core/src/com/asimov/piazzapanic/models/BinStation.java
+++ core/src/com/asimov/piazzapanic/models/BinStation.java
@@ -1,12 +1,19 @@
 package com.asimov.piazzapanic.models;
 
 public class BinStation extends CookingStation{
- 
-    public void bin() {
-
-        // needs to grab from top of ingedient station then delete item
-
+    private Ingredient ingredients = null;
+    public void place(Chef chef) throws Exception {
+        if (chef.stack.size() <= 0) {
+            throw new Exception("The chef's stack is empty.");
+        }
+        
     }
+    @Override
+    public void place(IngredientStack stack) {
+        ingredients = stack.place();
+        status = CookingStatus.available;
+    }
+
     @Override
     protected boolean canPlace(Ingredient ingredient) {
         return ingredient instanceof Ingredient;
