b8f9a8ecf2ae0023a1518d694d016f83bf89c3aa
Toby <toby@tobymeehan.com>
2023-01-26 14:07:45 +0000

    Fix ingredient stations
    Fixed grabbing multiple items

--- core/src/com/asimov/piazzapanic/BunStationSprite.java
+++ core/src/com/asimov/piazzapanic/BunStationSprite.java
@@ -5,16 +5,10 @@ import com.asimov.piazzapanic.models.*;
 import com.badlogic.gdx.graphics.Texture;
 
 public class BunStationSprite extends BaseIngredientStationSprite<IngredientStation>{
-
     Texture bunStation = new Texture("IngredientStations/Empty_Ingredient_Station.png");
+
     public BunStationSprite(DeltaTimer timer) {
         super(new IngredientStation<>(Bun::new), timer);
         setTexture(bunStation);
-    }
-
-    @Override
-    public void grab(IngredientStack stack) {
-        IngredientStation<Bun> IngredientStation = new IngredientStation<>(Bun::new);
-        IngredientStation.grab(stack);
     }
 }
--- core/src/com/asimov/piazzapanic/MeatStationSprite.java
+++ core/src/com/asimov/piazzapanic/MeatStationSprite.java
@@ -5,16 +5,10 @@ import com.asimov.piazzapanic.models.*;
 import com.badlogic.gdx.graphics.Texture;
 
 public class MeatStationSprite extends BaseIngredientStationSprite<IngredientStation>{
-
     Texture meatStation = new Texture("IngredientStations/Meat_Station.png");
+
     public MeatStationSprite(DeltaTimer timer) {
         super(new IngredientStation<>(Meat::new), timer);
         setTexture(meatStation);
-    }
-
-    @Override
-    public void grab(IngredientStack stack) {
-        IngredientStation<Meat> IngredientStation = new IngredientStation<>(Meat::new);
-        IngredientStation.grab(stack);
     }
 }
