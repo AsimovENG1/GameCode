57063c8248fa00c1bf89330097a9d62ca924c1da
Toby <toby@tobymeehan.com>
2023-01-28 17:04:32 +0000

    Update settings screen scaling
    Added table to settings screen

--- core/src/com/asimov/piazzapanic/SettingsScreen.java
+++ core/src/com/asimov/piazzapanic/SettingsScreen.java
@@ -10,12 +10,14 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
@@ -30,8 +32,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class SettingsScreen extends ScreenAdapter{
     final PiazzaPanic game;
-
     private Stage stage;
+    private Table table;
 
     private  static Integer senscore;
     private static Label senlabel;
@@ -45,7 +47,6 @@ public class SettingsScreen extends ScreenAdapter{
 
     private Texture setback = new Texture("layout/newppp.png");
     Sprite sprite = new Sprite(setback);
-    SpriteBatch batch = new SpriteBatch();
 
     SoundEffectControl soundEffectControl = new SoundEffectControl();
     MusicControl musicControl = new MusicControl();
@@ -56,79 +57,44 @@ public class SettingsScreen extends ScreenAdapter{
     public SettingsScreen(final PiazzaPanic game) {
         this.game = game;
 
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
         stage = new Stage(new ScreenViewport(), game.batch);
-
-        Skin mySkin1 = new Skin(Gdx.files.internal("skin/Holo-light-hdpi.json"));
-
-
-        Skin mySkin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
-        buclick2 = Gdx.audio.newSound(Gdx.files.internal("audio/Back-and-quit.wav"));
-
-
-        TextButton backButton = new TextButton("Back", mySkin);
-
-        backButton.setWidth(200);
-        backButton.setHeight(100);
-
-
-        backButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                SoundEffectControl.buclicking2();
-                game.setScreen(new MainMenuScreen(game));
-            }
-        });
-
-        stage.addActor(backButton);
-
-        backButton.setPosition(100, 100);
-
-        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.input.setInputProcessor(stage);
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 600);
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
 
-        // Style of the text of the heading
-        Label.LabelStyle style = new Label.LabelStyle();
-        style.font = game.font;
-        style.fontColor = Color.WHITE;
+        table.setDebug(game.debug);
+
+        Skin holoSkin = new Skin(Gdx.files.internal("skin/Holo-light-hdpi.json"));
+
+        buclick2 = Gdx.audio.newSound(Gdx.files.internal("audio/Back-and-quit.wav"));
+
+        Table settingsTable = new Table();
 
         // THE HEADING
-        Label label1 = new Label("Settings", style);
-        label1.setSize((float)Gdx.graphics.getWidth(), 200.0F);
+        Label label1 = new Label("Settings", game.skin);
         label1.setFontScale(3.0F);
-        label1.setPosition(0, 850.0F);
-        label1.setAlignment(Align.center);
-        this.stage.addActor(label1);
+        settingsTable.add(label1);
 
-        //label styles
-        Label.LabelStyle style1 = new Label.LabelStyle();
-        style1.font = game.font;
-        style1.fontColor = Color.WHITE;
+        settingsTable.row();
 
         // FOR PERMANENT STUFF
         muscore = 0;
         effectscore = 0;
         Preferences prefs = Gdx.app.getPreferences("myprefs");
 
-
         // MUSIC
-        Label Mlabel = new Label("Music", style1);
-        Mlabel.setSize((float)Gdx.graphics.getWidth(), 200.0F);
+        Label Mlabel = new Label("Music", game.skin);
         Mlabel.setFontScale(2.0F);
-        Mlabel.setPosition(0 ,700.0F);
-        Mlabel.setAlignment(Align.center);
-        this.stage.addActor(Mlabel);
+        settingsTable.add(Mlabel);
 
+        settingsTable.row();
 
         // VOLUME UP
-        TextButton volumeup = new TextButton("UP",mySkin1);
-        volumeup.setWidth(100);
-        volumeup.setHeight(100);
-        volumeup.setPosition(820.0F, 595.0F);
-        this.stage.addActor(volumeup);
-
+        TextButton volumeup = new TextButton("UP", holoSkin);
         volumeup.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event,float x, float y){
@@ -144,12 +110,7 @@ public class SettingsScreen extends ScreenAdapter{
         });
 
         // VOLUME DOWN
-        TextButton volumedown = new TextButton("Down", mySkin1);
-        volumedown.setWidth(100);
-        volumedown.setHeight(100);
-        volumedown.setPosition(1000.0F, 595.0F);
-        this.stage.addActor(volumedown);
-
+        TextButton volumedown = new TextButton("Down", holoSkin);
         volumedown.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event,float x, float y){
@@ -165,32 +126,21 @@ public class SettingsScreen extends ScreenAdapter{
 
         // MUSIC INTEGER
         muscore = prefs.getInteger("muscore");
-        mulabel =new Label(String.format("%01d", muscore), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        mulabel =new Label(String.format("%01d", muscore), game.skin);
         mulabel.setFontScale(2.0F);
-        mulabel.setAlignment(Align.center);
-        mulabel.setPosition(950F, 650.0F);
-        this.stage.addActor(mulabel);
 
+        settingsTable.add(volumedown, mulabel, volumeup);
 
-
-
+        settingsTable.row();
 
         // SOUND EFFECTS
-        Label SElabel = new Label("Sound Effects", style1);
-        SElabel.setSize((float)Gdx.graphics.getWidth(), 200.0F);
-        SElabel.setFontScale(2.0F);
-        SElabel.setPosition(0 ,400.0F);
-        SElabel.setAlignment(Align.center);
-        this.stage.addActor(SElabel);
+        Label SElabel = new Label("Sound Effects", game.skin);
+        settingsTable.add(SElabel);
 
+        settingsTable.row();
 
         // SOUND EFFECT UP
-        TextButton effectup = new TextButton("UP",mySkin1);
-        effectup.setWidth(100);
-        effectup.setHeight(100);
-        effectup.setPosition(820.0F, 250.0F);
-        this.stage.addActor(effectup);
-
+        TextButton effectup = new TextButton("UP",holoSkin);
         effectup.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event,float x, float y){
@@ -205,13 +155,8 @@ public class SettingsScreen extends ScreenAdapter{
             }
         });
 
-
         // SOUND EFFECT DOWN
-        TextButton effectdown = new TextButton("Down", mySkin1);
-        effectdown.setWidth(100);
-        effectdown.setHeight(100);
-        effectdown.setPosition(1000.0F, 250.0F);
-        this.stage.addActor(effectdown);
+        TextButton effectdown = new TextButton("Down", holoSkin);
 
         effectdown.addListener(new ClickListener(){
             @Override
@@ -229,21 +174,30 @@ public class SettingsScreen extends ScreenAdapter{
 
         // SOUND EFFECT INTEGER
         effectscore = prefs.getInteger("effectscore");
-        effectlabel =new Label(String.format("%01d", effectscore), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        effectlabel =new Label(String.format("%01d", effectscore), game.skin);
         effectlabel.setFontScale(2.0F);
-        effectlabel.setPosition(950.0F, 300.0F);
-        effectlabel.setAlignment(Align.center);
-        this.stage.addActor(effectlabel);
 
+        settingsTable.add(effectdown, effectlabel, effectup);
 
+        settingsTable.row();
 
+        TextButton backButton = new TextButton("Back", game.skin);
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                SoundEffectControl.buclicking2();
+                game.setScreen(new MainMenuScreen(game));
+            }
+        });
 
+        table.add(settingsTable).expand().center();
 
+        table.row();
+
+        table.add(backButton).expandX().left().bottom();
+
+        table.pad(10);
     }
-
-
-
-
 
     @Override
     public void resize(int width, int height) {
@@ -253,14 +207,8 @@ public class SettingsScreen extends ScreenAdapter{
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Gdx.graphics.getDeltaTime());
+        stage.act(delta);
         stage.draw();
-        batch.begin();
-        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        sprite.setPosition(0,0);
-        sprite.setAlpha(0.1f);
-        sprite.draw(batch);
-        batch.end();
     }
 
     @Override
