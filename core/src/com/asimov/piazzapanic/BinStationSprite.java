6c56bbf46e9b527070a7d74ada3edb0d42ae58f4
Isioma Offokansi <ioo510.york.ac.uk>
2023-01-29 17:19:45 +0000

    Add sfx for all interactions except for flipping

--- core/src/com/asimov/piazzapanic/BinStationSprite.java
+++ core/src/com/asimov/piazzapanic/BinStationSprite.java
@@ -2,10 +2,14 @@ package com.asimov.piazzapanic;
 
 import com.asimov.piazzapanic.deltatimer.DeltaTimer;
 import com.asimov.piazzapanic.models.BinStation;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 public class BinStationSprite extends BaseCookingStationSprite<BinStation> {
     private final Texture Texture = new Texture("Bin.png");
+
+    private Sound trash;
     private DeltaTimer timer;
 
     public BinStationSprite(DeltaTimer timer) {
@@ -17,5 +21,6 @@ public class BinStationSprite extends BaseCookingStationSprite<BinStation> {
     @Override
     protected void placed() {
         timer.start(0.5f, () -> model.open());
+        trash = Gdx.audio.newSound((Gdx.files.internal(("audio/mixkit-falling-on-foil-748.wav"))));
     }
 }
--- core/src/com/asimov/piazzapanic/GrillStationSprite.java
+++ core/src/com/asimov/piazzapanic/GrillStationSprite.java
@@ -2,6 +2,9 @@ package com.asimov.piazzapanic;
 
 import com.asimov.piazzapanic.deltatimer.DeltaTimer;
 import com.asimov.piazzapanic.models.FryingStation;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 public class GrillStationSprite extends BaseCookingStationSprite<FryingStation> {
@@ -11,6 +14,9 @@ public class GrillStationSprite extends BaseCookingStationSprite<FryingStation> 
     private final Texture GrillReadyTexture = new Texture("burgerGrillReady.png"); // burger is ready to be flipped
     private final Texture GrillFlipedTexture = new Texture("burgerGrillFliped.png"); // burger is fliped
     private final Texture GrillDoneTexture = new Texture("burgerGrillDone.png"); // burger is ready to be taken off
+
+    private Music frying = Gdx.audio.newMusic((Gdx.files.internal(("audio/frying-3.wav"))));
+
     private DeltaTimer timer;
 
     public GrillStationSprite(DeltaTimer timer) {
@@ -22,6 +28,8 @@ public class GrillStationSprite extends BaseCookingStationSprite<FryingStation> 
     public void placed() {
         setTexture(GrillTexture);
         timer.start(5, () -> readyToFlip());
+        frying.setLooping(true);
+        frying.play();
     }
     public void readyToFlip() {
         setTexture(GrillReadyTexture);
@@ -35,6 +43,7 @@ public class GrillStationSprite extends BaseCookingStationSprite<FryingStation> 
         model.flip();
         setTexture(GrillFlipedTexture);
         timer.start(5, () -> readyToPickUp());
+        timer.start(5, () -> frying.setLooping(false));
     }
     public void readyToPickUp() {
         setTexture(GrillDoneTexture);
