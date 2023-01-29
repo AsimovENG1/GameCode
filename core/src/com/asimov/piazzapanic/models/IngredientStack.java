467d16528704b4e34d9bb8b9ada1fcc1b29d52ef
Toby <toby@tobymeehan.com>
2023-01-29 10:34:37 +0000

    Add javadocs for ingredient station/stack

--- core/src/com/asimov/piazzapanic/models/IngredientStack.java
+++ core/src/com/asimov/piazzapanic/models/IngredientStack.java
@@ -4,7 +4,14 @@ import com.badlogic.gdx.utils.Array;
 
 import java.util.Iterator;
 
+/**
+ * A stack of ingredients held by a chef. Implements Iterable if needed.
+ */
 public class IngredientStack implements Iterable<Ingredient> {
+
+    /**
+     * The stack items in array form.
+     */
     public Array<Ingredient> items = new Array<>();
 
     @Override
@@ -12,22 +19,41 @@ public class IngredientStack implements Iterable<Ingredient> {
         return items.iterator();
     }
 
+    /**
+     * Adds the specified ingredient to the top of the stack.
+     * @param ingredient Ingredient to add.
+     */
     public void grab(Ingredient ingredient) {
         items.add(ingredient);
     }
 
+    /**
+     * Removes and returns the ingredient at the top of the stack.
+     * @return Ingredient to be placed.
+     */
     public Ingredient place() {
         return items.pop();
     }
 
+    /**
+     * Returns the ingredient at the top of the stack, but does not remove it.
+     * @return Ingredient currently at the top of the stack.
+     */
     public Ingredient peek() {
         return items.peek();
     }
 
+    /**
+     * Removes all ingredients from the stack.
+     */
     public void clear() {
         items.clear();
     }
 
+    /**
+     * Returns the number of ingredients held in the stack.
+     * @return The size of the stack.
+     */
     public int size() {
         return items.size;
     }
--- core/src/com/asimov/piazzapanic/models/IngredientStation.java
+++ core/src/com/asimov/piazzapanic/models/IngredientStation.java
@@ -1,21 +1,25 @@
 package com.asimov.piazzapanic.models;
 
-import java.util.concurrent.TimeUnit;
-
+/**
+ * An ingredient station for the specified ingredient.
+ * @param <T> Type of ingredient the station provides.
+ */
 public class IngredientStation<T extends Ingredient> {
     private IngredientSource<T> ingredient;
 
+    /**
+     * Constructor, requires method to create new ingredient.
+     * @param ingredient Ingredient constructor method, for example () -> new Lettuce().
+     */
     public IngredientStation(IngredientSource<T> ingredient) {
         this.ingredient = ingredient;
     }
 
+    /**
+     * Grabs a new ingredient from the station and places it at the top of the specified stack. Ensure stack is not full before using.
+     * @param stack Stack to add ingredient to.
+     */
     public void grab(IngredientStack stack) {
-        try {
-            stack.grab(ingredient.create());
-            //TimeUnit.SECONDS.sleep(1);
-        }
-        catch (Exception e) {
-            System.out.println(e);
-        }
+        stack.grab(ingredient.create());
     }
 }
